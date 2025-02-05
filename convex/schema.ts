51d2851058d2e909aa12cd1583d52b5980635c39
Luan <ventobo123@gmail.com>
2025-02-05 17:53:58 -0300

    Types

--- convex/schema.ts
+++ convex/schema.ts
@@ -8,8 +8,8 @@ export default defineSchema({
     name: v.string(),
     // isPro: v.boolean(),
     proSince: v.optional(v.number()),
-    lemonSqueezyCustomerId: v.optional(v.string()),
-    lemonSqueezyOrderId: v.optional(v.string()),
+    // lemonSqueezyCustomerId: v.optional(v.string()),
+    // lemonSqueezyOrderId: v.optional(v.string()),
   }).index("by_user_id", ["userId"]),
 
   codeExecution: defineTable({
