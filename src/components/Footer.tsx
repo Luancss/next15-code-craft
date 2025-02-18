bb866010ae26a7b285d7d36d014de9cab496ba0f
Luan <ventobo123@gmail.com>
2025-02-18 02:48:51 -0300

    Button on header and information on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,26 @@
-import { Blocks } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="relative border-t border-gray-800/50 mt-auto">
-      <div className="absolute inset-x-0 -top-px h-px bg-gradient-to-r from-transparent via-gray-900 to-transparent" />
-      <div className="max-w-7xl mx-auto px-4 py-8">
-        <div className="flex flex-col md:flex-row items-center justify-between gap-4">
-          <div className="flex items-center gap-2 text-gray-400">
-            <Blocks className="size-5" />
-            <span>Built for developers, by developers</span>
-          </div>
-          <div className="flex items-center gap-6">
-            <Link
-              href="/support"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
-            >
-              Support
-            </Link>
-            <Link
-              href="/privacy"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
-            >
-              Privacy
-            </Link>
-            <Link
-              href="/terms"
-              className="text-gray-400 hover:text-gray-300 transition-colors"
-            >
-              Terms
-            </Link>
-          </div>
+    <footer className="border-t border-gray-800/50 w-full flex items-center justify-center">
+      <div className="mx-auto px-4 py-8">
+        <div className="flex items-center gap-2 text-gray-400">
+          <Image
+            alt="Luancss"
+            src="https://avatars.githubusercontent.com/u/104950187?v=4"
+            width={50}
+            height={50}
+            className="rounded-full"
+          />
+          <p className="text-gray-300">Built for</p>
+          <Link
+            href="https://github.com/Luancss"
+            target="_blank"
+            className="text-zinc-400 underline"
+          >
+            Luancss.
+          </Link>
         </div>
       </div>
     </footer>
